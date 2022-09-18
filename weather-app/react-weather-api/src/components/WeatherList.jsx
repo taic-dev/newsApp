56f4fae1be9a3d21954c2dcfe3bbbeb4109fc284
“taic-dev” <c.t.o.taishi.0530@gmail.com>
2022-09-19 00:18:46 +0900

    css修正

--- weather-app/react-weather-api/src/components/WeatherList.jsx
+++ weather-app/react-weather-api/src/components/WeatherList.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import CloudIcon from '@mui/icons-material/Cloud';
+import UmbrellaIcon from '@mui/icons-material/Umbrella';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+
 
 export const WeatherList = [
-            { num:0, name:"晴天" },
-            { num:1, name:"晴れ" },
-            { num:2, name:"おおむね晴れ" },
-            { num:3, name:"ときどき曇り" },
-            { num:61,name:"小雨" },
-            { num:62, name:"雨" },
-            { num:63, name:"大雨" },
-        ]
+    { 
+        num: 0, 
+        name: "晴天",
+        icon: <WbSunnyIcon />
+    },
+    { 
+        num: 1, 
+        name: "晴れ",
+        icon: <WbSunnyIcon />
+    },
+    { 
+        num: 2, 
+        name: "おおむね晴れ",
+        icon: <WbSunnyIcon />
+    },
+    { 
+        num: 3, 
+        name: "曇り",
+        icon: <CloudIcon />
+    },
+    { 
+        num: 61,
+        name: "小雨",
+        icon: <UmbrellaIcon />
+
+    },
+    {
+        num: 62,
+        name: "雨",
+        icon: <BeachAccessIcon />
+    },
+    { 
+        num: 63,
+        name: "大雨",
+        icon: <BeachAccessIcon />
+    },
+]
     
