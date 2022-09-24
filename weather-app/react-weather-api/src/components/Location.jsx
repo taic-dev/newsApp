c185f2367e7a8f22755b749bbad7b88d0d9dbff8
“taic-dev” <c.t.o.taishi.0530@gmail.com>
2022-09-24 17:01:41 +0900

    市区町村の情報取得 / Selectボックスの作成

--- weather-app/react-weather-api/src/components/Location.jsx
+++ weather-app/react-weather-api/src/components/Location.jsx
@@ -1,30 +1,37 @@
-import { Button, MenuItem, Select, TextField } from '@material-ui/core'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { Button, MenuItem, Select, TextField } from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Location = () => {
+  const [prefectures, setPrefectures] = useState([""]);
+  const [city, setCity] = useState([""]);
+  const [selectPrefectures, setSelectPrefectures] = useState("東京都");
+  const [selectCity, setSelectCity] = useState("千代田区");
 
-    const [prefectures,setPrefectures] = useState(['']);
-    const [selectPrefectures,setSelectPrefectures] = useState('東京都');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const prefecturesURL = "http://localhost:3001/prefectures";
+        let prefecturesResponse = await axios.get(prefecturesURL);
+        setPrefectures(prefecturesResponse.data.response.prefecture);
+    
+        const cityURL = "http://localhost:3001/city";
+        let cityResponse = await axios.get(cityURL);
+        setCity(cityResponse.data.response.location)
 
-    useEffect(()=>{
-        const fetchData = async () => {
-            try{
-                const baseURL = "http://localhost:3001/prefectures";
-                let response = await axios.get(baseURL);
-                setPrefectures(response.data.response.prefecture);
-            }catch(e){
-                console.log(e);
-            }
-        }
-        fetchData();
-    },[]);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
 
-    const changePrefectures = e => setSelectPrefectures(e.target.value);
+  const changePrefectures = (e) => setSelectPrefectures(e.target.value);
+  const changeCity = (e) => setSelectCity(e.target.value);
 
   return (
     <main>
-      <div className='location-main'>
+      <div className="location-main">
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
@@ -32,30 +39,34 @@ const Location = () => {
           value={selectPrefectures}
           onChange={changePrefectures}
         >
-            {prefectures.map((prefecture,index)=>{
-                return(
-                    <MenuItem key={index} value={prefecture}>
-                        {prefecture}
-                    </MenuItem>
-                );
-            })}
+          {prefectures.map((prefecture, index) => {
+            return (
+              <MenuItem key={index} value={prefecture}>
+                {prefecture}
+              </MenuItem>
+            );
+          })}
         </Select>
 
-        <TextField
-            id="standard-select-currency"
-            select
-            label="市"
-            helperText="市を選択してください"
-            variant="standard"
-            >
-            <MenuItem>
-                テスト
-            </MenuItem>
-        </TextField>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          label="市区町村"
+          value={selectCity}
+          onChange={changeCity}
+        >
+          {city.map((cityObj, index) => {
+            return (
+              <MenuItem key={index} value={cityObj.city}>
+                {cityObj.city}
+              </MenuItem>
+            );
+          })}
+        </Select>
         <Button variant="contained">設定する</Button>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Location
+export default Location;
